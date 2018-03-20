75f19c58d670743a3672c56b8826b75f7b333f3a
Marc5567 <37524508+Marc5567@users.noreply.github.com>
2018-03-20 09:44:03 +0800

    Add files via upload

--- direwolf.py
+++ direwolf.py
@@ -59,7 +59,6 @@ UPDATE_DELAY = 60  # update every minute
 
 ITEM_SOURCE = "https://raw.githubusercontent.com/Marc5567/dire-wolf/master/items.txt"
 ITEM_DIVIDER = "***"  # a string that divides distinct items.
-IGNORED_ENTRIES = 0  # a number of entries to ignore (in case of an index, etc)
 META_LINES = 0  # the number of lines of meta info each item has
 
 items = []
@@ -84,17 +83,19 @@ async def update_sources_loop():
 
 
 async def update_sources():
+    ITEM_SOURCE = "https://raw.githubusercontent.com/Marc5567/dire-wolf/master/items.txt"
     async with aiohttp.ClientSession() as session:
         async with session.get(ITEM_SOURCE) as resp:
+            print(ITEM_SOURCE)
             text = await resp.text()
             if 399 < resp.status < 600:  # 4xx, 5xx errors
                 raise Exception(f"Failed to update items: {text}")
 
-            raw_items = [t.strip() for t in text.split(ITEM_DIVIDER)][IGNORED_ENTRIES:]  # filter out index
+            raw_items = [t.strip() for t in text.split(ITEM_DIVIDER)]  # filter out index
 
             for item in raw_items:
                 lines = item.split('\n')
-                name = lines[0].strip('## ')
+                name = lines[0].strip('### ')
                 meta = '\n'.join(lines[1::])
                 desc = '\n'.join(lines)
 
@@ -106,14 +107,13 @@ async def update_sources():
             print("Updated items!")
 
 @client.command(pass_context=True)
-async def item(ctx, *, name):
-    #Looks up a Dungeon World item.
+async def item(ctx, *, name=''):
+    """Looks up a homebrew item."""
     result = search(items, 'name', name)
     if result is None:
         return await client.say('Item not found.')
     strict = result[1]
     results = result[0]
-
     if strict:
         result = results
     else:
@@ -121,17 +121,14 @@ async def item(ctx, *, name):
             result = results[0]
         else:
             result = await get_selection(ctx, [(r['name'], r) for r in results])
-            if result is None: return await bot.say('Selection timed out or was cancelled.')
-
+            if result is None: return await client.say('Selection timed out or was cancelled.')
     embed = EmbedWithAuthor(ctx)
     embed.title = result['name']
-    embed.description = result['meta']
-    desc = result['desc']
-    desc = [desc[i:i + 1024] for i in range(0, len(desc), 1024)]
-    embed.add_field(name="Description", value=desc[0])
-    for piece in desc[1:]:
+    meta = result['meta']
+    meta2 = [meta[i:i + 1024] for i in range(2048, len(meta), 1024)]
+    embed.description = meta[0:2048]
+    for piece in meta2:
         embed.add_field(name="\u200b", value=piece)
-
     await client.say(embed=embed)
 
 client.run('NDIwMDkxNDE2Mzc1NDU5ODcw.DX5p3A.s7Qla8wABEvNhaHP-AO2gvV3UKE')
