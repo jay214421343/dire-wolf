b137230d9003778ccd10d57ef8e0685a8aa859e1
Marc5567 <37524508+Marc5567@users.noreply.github.com>
2018-03-21 07:32:46 +0800

    Update direwolf.py

--- direwolf.py
+++ direwolf.py
@@ -1,4 +1,3 @@
-# this is a test instance of Dire Wolf while trying to implement multiple search sources
 import discord
 import asyncio
 import os
@@ -20,18 +19,18 @@ async def on_ready():
     print('--------')
     print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
     print('--------')
-    print('Support Discord Server: https://discord.gg/FNNNgqb')
+    print('Support Discord Server: https://discord.gg/q9bmCZJ')
     print('Github Link: https://github.com/Marc5567/dire-wolf')
     print('--------')
-    print('You are running Dire Wolf v1.0.1')
+    print('You are running Dire Wolf v1.0.2')
     print('Created by Marcus#3244')
     print('--------')
-    client.loop.create_task(update_sources_loop())
+    client.loop.create_task(update_sources_loop()) # creates update loop for data sources
     return await client.change_presence(game=discord.Game(name='Dungeon World'))
 
 @client.command()
 async def hack(*args):
-    """Rolls Hack and Slash""" # to be rewritten as move with subcommands for individual moves
+    """Rolls Hack and Slash""" # to be rewritten as move with subcommands for all moves
     try:
         mod = int(args[0])
     except:
@@ -52,15 +51,20 @@ async def hack(*args):
     embed.set_footer(text="Basic Move, 58")
     await client.say(embed=embed)
 
+# everything below is for info lookup
 UPDATE_DELAY = 60 * 60  # update every hour
 
 ITEM_SOURCE = "https://raw.githubusercontent.com/Marc5567/dire-wolf/master/items.txt"
 MONSTER_SOURCE = "https://raw.githubusercontent.com/Marc5567/dire-wolf/master/monsters"
+SPELL_SOURCE = "https://raw.githubusercontent.com/Marc5567/dire-wolf/master/spells"
+TAG_SOURCE = "https://raw.githubusercontent.com/Marc5567/dire-wolf/master/tags"
 ITEM_DIVIDER = "***"  # a string that divides distinct items.
 META_LINES = 0  # the number of lines of meta info each item has
 
 items = []
 monsters = []
+spells = []
+tags = []
 
 @client.event
 async def on_command_error(error, ctx):
@@ -80,13 +84,10 @@ async def update_sources_loop():
     except asyncio.CancelledError:
         pass
 
-
 async def update_sources():
-    ITEM_SOURCE = "https://raw.githubusercontent.com/Marc5567/dire-wolf/master/items.txt"
-    MONSTER_SOURCE = "https://raw.githubusercontent.com/Marc5567/dire-wolf/master/monsters"
+    # item indexing
     async with aiohttp.ClientSession() as session:
         async with session.get(ITEM_SOURCE) as resp:
-            print(ITEM_SOURCE)
             text = await resp.text()
             if 399 < resp.status < 600:  # 4xx, 5xx errors
                 raise Exception(f"Failed to update items: {text}")
@@ -100,13 +101,11 @@ async def update_sources():
                 desc = '\n'.join(lines)
 
                 items.append({'name': name, 'meta': meta, 'desc': desc})
-                print(f"Indexed item {name}.")
 
             print("Updated items!")
-
+    # monster indexing
     async with aiohttp.ClientSession() as session:
         async with session.get(MONSTER_SOURCE) as resp:
-            print(MONSTER_SOURCE)
             text = await resp.text()
             if 399 < resp.status < 600:  # 4xx, 5xx errors
                 raise Exception(f"Failed to update monsters: {text}")
@@ -120,13 +119,48 @@ async def update_sources():
                 desc = '\n'.join(lines)
 
                 monsters.append({'name': name, 'meta': meta, 'desc': desc})
-                print(f"Indexed monster {name}.")
 
             print("Updated monsters!")
+    # spell indexing
+    async with aiohttp.ClientSession() as session:
+        async with session.get(SPELL_SOURCE) as resp:
+            text = await resp.text()
+            if 399 < resp.status < 600:  # 4xx, 5xx errors
+                raise Exception(f"Failed to update spells: {text}")
+
+            raw_spells = [t.strip() for t in text.split(ITEM_DIVIDER)]  # filter out index
+
+            for spell in raw_spells:
+                lines = spell.split('\n')
+                name = lines[0].strip('### ')
+                meta = '\n'.join(lines[1::])
+                desc = '\n'.join(lines)
+
+                spells.append({'name': name, 'meta': meta, 'desc': desc})
+
+            print("Updated spells!")
+    # tag indexing
+    async with aiohttp.ClientSession() as session:
+        async with session.get(TAG_SOURCE) as resp:
+            text = await resp.text()
+            if 399 < resp.status < 600:  # 4xx, 5xx errors
+                raise Exception(f"Failed to update tags: {text}")
+
+            raw_tags = [t.strip() for t in text.split(ITEM_DIVIDER)]  # filter out index
+
+            for tag in raw_tags:
+                lines = tag.split('\n')
+                name = lines[0].strip('### ')
+                meta = '\n'.join(lines[1::])
+                desc = '\n'.join(lines)
+
+                tags.append({'name': name, 'meta': meta, 'desc': desc})
+
+            print("Updated tags!")
 
 @client.command(pass_context=True)
 async def item(ctx, *, name=''):
-    """Looks up a Dungeon World item."""
+    """Looks up a Dungeon World item.\nLists by category available by searching \"Weapons\", \"Armor\", etc.\nFull list of categories can be viewed by searching \"Item Categories\"."""
     result = search(items, 'name', name)
     if result is None:
         return await client.say('Item not found.')
@@ -151,7 +185,7 @@ async def item(ctx, *, name=''):
 
 @client.command(pass_context=True)
 async def monster(ctx, *, name=''):
-    """Looks up a Dungeon World monster."""
+    """Looks up a Dungeon World monster.\nSearch with environment name for list of monsters found in that environment.\n\"Environment List\" for a list of available environments."""
     result = search(monsters, 'name', name)
     if result is None:
         return await client.say('Monster not found.')
@@ -174,8 +208,54 @@ async def monster(ctx, *, name=''):
         embed.add_field(name="\u200b", value=piece)
     await client.say(embed=embed)
 
-client.run('NDIwMDkxNDE2Mzc1NDU5ODcw.DX5p3A.s7Qla8wABEvNhaHP-AO2gvV3UKE')
+@client.command(pass_context=True)
+async def spell(ctx, *, name=''):
+    """Looks up a Dungeon World spell.\nSearch \"Wizard\" or \"Cleric\" for class spell lists."""
+    result = search(spells, 'name', name)
+    if result is None:
+        return await client.say('Spell not found.')
+    strict = result[1]
+    results = result[0]
+    if strict:
+        result = results
+    else:
+        if len(results) == 1:
+            result = results[0]
+        else:
+            result = await get_selection(ctx, [(r['name'], r) for r in results])
+            if result is None: return await client.say('Selection timed out or was cancelled.')
+    embed = EmbedWithAuthor(ctx)
+    embed.title = result['name']
+    meta = result['meta']
+    meta2 = [meta[i:i + 1024] for i in range(2048, len(meta), 1024)]
+    embed.description = meta[0:2048]
+    for piece in meta2:
+        embed.add_field(name="\u200b", value=piece)
+    await client.say(embed=embed)
 
-# Please join this Discord server if you need help: https://discord.gg/FNNNgqb
-# The help command is currently set to be not be Direct Messaged.
-# If you would like to change that, change "pm_help = False" to "pm_help = True" on line 9.
+@client.command(pass_context=True)
+async def tag(ctx, *, name=''):
+    """Looks up a Dungeon World item tag.\nSearch \"Item Tags\" or \"Monster Tags\" to view respective lists.""""
+    result = search(tags, 'name', name)
+    if result is None:
+        return await client.say('Tag not found.')
+    strict = result[1]
+    results = result[0]
+    if strict:
+        result = results
+    else:
+        if len(results) == 1:
+            result = results[0]
+        else:
+            result = await get_selection(ctx, [(r['name'], r) for r in results])
+            if result is None: return await client.say('Selection timed out or was cancelled.')
+    embed = EmbedWithAuthor(ctx)
+    embed.title = result['name']
+    meta = result['meta']
+    meta2 = [meta[i:i + 1024] for i in range(2048, len(meta), 1024)]
+    embed.description = meta[0:2048]
+    for piece in meta2:
+        embed.add_field(name="\u200b", value=piece)
+    await client.say(embed=embed)
+
+client.run('TOKEN')
